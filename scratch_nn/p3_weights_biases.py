3cfd3d3984a95304c3ccb9860f41f0d733035e63
Ryo Nakamura <ryo.nakamura@aii.cloud>
2022-07-07 10:37:59 +0900

    nn learning

---
+++ scratch_nn/p3_weights_biases.py
@@ -0,0 +1,21 @@
+inputs = [1,2,3,4]
+weights = [[1,2,3,4],
+           [1,0.2,3,0.5],
+           [1,2,3,4]]
+biases = [1,2,3]
+
+# print(list(zip(inputs,weights)))
+
+# for n_input,weight in zip(inputs,weights):
+#     print(n_input*weight)
+layer_outputs = []
+for n_weight, n_bias in zip(weights,biases):
+    neuron_output = 0
+    for n_input,weight in zip(inputs,n_weight):
+        neuron_output += n_input*weight
+    neuron_output += n_bias
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
+
+# print(layer_outputs)
